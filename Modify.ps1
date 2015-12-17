ebcfb99c7698d21374696d8218346bc80c2ae80e
jgb6345 <jgb6345@rit.edu>
2015-12-17 11:49:14 -0500

    Updated to sort by user who owns files

--- Modify.ps1
+++ Modify.ps1
@@ -1,6 +1,7 @@
 #Julie Brady, Morgan Maroni, Shawn Winner
 #CSEC 475 Final Project
-#Permissions stuff
+
+#Permissions Information
 #Finding if user is the admin
 $isAdmin = (new-object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole("Administrators")
 
@@ -45,6 +46,9 @@ $DirPath = "C:\"
 $OutFile = "modifytool.csv"
 $mode = "csv"
 
+#Declaring current user
+$user = '*{0}*' -f $env:USERNAME
+
 #checks for help command and shows help menu if used
 foreach ($n in $args){
 	if ($n -eq "-h" -or $n -eq "--help"){ 
@@ -58,19 +62,22 @@ foreach ($n in $args){
 	
 	if($n -like '*--startdate==*'){
 		$StartDate = Get-Date -Date $n.Substring(13)
-		}
+	}
 	elseif ($n -like '*--enddate==*'){
 		$EndDate = Get-Date -Date $n.Substring(11)
-		}
+	}
 	elseif ($n -like '*--searchtype==*'){
 		$n = $n.Substring(14)
-		if (($n -lt 8) -and ($n -ge 0)){$SearchType = $n} #Just Create Date Compare
+		if (($n -lt 9) -and ($n -ge 0)){$SearchType = $n} #Just Create Date Compare
 		
 		else {Write-Host "Error: Search Type invalid, using default of 1"}
-		}
+	}
 	elseif ($n -like '*--dirpath==*'){
 		$DirPath = $n.Substring(11)
-		}
+	}
+	elseif ($n -like '*--user==*'){
+		$user = '*{0}*' -f $n
+	}
 	elseif ($n -like '*--outfile==*'){
 		$OutFile = $n.Substring(11)
 		$index = $OutFile.IndexOf(".csv")
@@ -83,7 +90,7 @@ foreach ($n in $args){
 		else{
 			$mode = "text"
 		}
-		}
+	}
 }
 
 #Informs the user of the defaults chosen
@@ -131,6 +138,9 @@ if($mode -eq "csv"){
 	elseif($SearchType -eq 7) {
 		Get-ChildItem $DirPath -Recurse | where-object{($_.CreationTime -gt $StartDate) -and ($_.CreationTime -lt $EndDate) -and ($_.LastAccessTime -gt $StartDate) -and ($_.LastAccessTime -lt $EndDate) -and($_.LastWriteTime -gt $StartDate) -and ($_.LastWriteTime -lt $EndDate)} | Sort-Object -Property lastwritetime -Descending | select-object FullName, LastWriteTime, LastAccessTime, CreationTime, Length, @{N='Owner';E={$_.GetAccessControl().Owner}}, @{N='Access';E={$_.GetAccessControl().Accesstostring}} | export-csv $OutFile -force
 	}
+	elseif($SearchType -eq 8){
+		Get-ChildItem $DirPath -Recurse | where-object{$_.GetAccessControl().Owner -like $user} | Sort-Object -Property lastwritetime -Descending | select-object FullName, LastWriteTime, LastAccessTime, CreationTime, Length, @{N='Owner';E={$_.GetAccessControl().Owner}}, @{N='Access';E={$_.GetAccessControl().Accesstostring}} | out-file -filepath $OutFile -force
+	}
 }
 #Writes all the Files to a text file
 elseif($mode -eq "text"){
@@ -157,6 +167,9 @@ elseif($mode -eq "text"){
 	}
 	elseif($SearchType -eq 7) {
 		Get-ChildItem $DirPath -Recurse | where-object{($_.CreationTime -gt $StartDate) -and ($_.CreationTime -lt $EndDate) -and ($_.LastAccessTime -gt $StartDate) -and ($_.LastAccessTime -lt $EndDate) -and($_.LastWriteTime -gt $StartDate) -and ($_.LastWriteTime -lt $EndDate)} | Sort-Object -Property lastwritetime -Descending | select-object FullName, LastWriteTime, LastAccessTime, CreationTime, Length, @{N='Owner';E={$_.GetAccessControl().Owner}}, @{N='Access';E={$_.GetAccessControl().Accesstostring}} | out-file -filepath $OutFile -force
+	}
+	elseif($SearchType -eq 8){
+		Get-ChildItem $DirPath -Recurse | where-object{$_.GetAccessControl().Owner -like $user} | Sort-Object -Property lastwritetime -Descending | select-object FullName, LastWriteTime, LastAccessTime, CreationTime, Length, @{N='Owner';E={$_.GetAccessControl().Owner}}, @{N='Access';E={$_.GetAccessControl().Accesstostring}} | out-file -filepath $OutFile -force
 	}
 }
 #Writes results only to console
@@ -185,7 +198,9 @@ elseif($mode -eq "none"){
 	elseif($SearchType -eq 7) {
 		Get-ChildItem $DirPath -Recurse | where-object{($_.CreationTime -gt $StartDate) -and ($_.CreationTime -lt $EndDate) -and ($_.LastAccessTime -gt $StartDate) -and ($_.LastAccessTime -lt $EndDate) -and($_.LastWriteTime -gt $StartDate) -and ($_.LastWriteTime -lt $EndDate)} | Sort-Object -Property lastwritetime -Descending | select-object FullName, LastWriteTime, LastAccessTime, CreationTime, Length, @{N='Owner';E={$_.GetAccessControl().Owner}}, @{N='Access';E={$_.GetAccessControl().Accesstostring}} | out-file -filepath $OutFile -force
 	}
-
+	elseif($SearchType -eq 8){
+		Get-ChildItem $DirPath -Recurse | where-object{$_.GetAccessControl().Owner -like $user} | Sort-Object -Property lastwritetime -Descending | select-object FullName, LastWriteTime, LastAccessTime, CreationTime, Length, @{N='Owner';E={$_.GetAccessControl().Owner}}, @{N='Access';E={$_.GetAccessControl().Accesstostring}} | out-file -filepath $OutFile -force
+	}
 }
 
 #Informs user that the script has finished
